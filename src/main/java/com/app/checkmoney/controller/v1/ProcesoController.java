d39f8e027e9fc87ce8b691478862ac5969ebce52
Escudero183 <49972636+Escudero183@users.noreply.github.com>
2022-04-19 11:29:51 -0500

    Add microservicios Cliente
    Se desarrolló los microservicios de Cliente: get, post, put, delete

--- src/main/java/com/app/checkmoney/controller/v1/ProcesoController.java
+++ src/main/java/com/app/checkmoney/controller/v1/ProcesoController.java
@@ -29,7 +29,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.checkmoney.config.exception.ApiCheckMoneyException;
 import com.app.checkmoney.config.exception.RestException;
 import com.app.checkmoney.config.security.model.JwtUser;
+import com.app.checkmoney.model.Cliente;
 import com.app.checkmoney.model.TipoCambio;
+import com.app.checkmoney.service.ClienteService;
 import com.app.checkmoney.service.TipoCambioService;
 
 import io.swagger.annotations.ApiOperation;
@@ -46,6 +48,9 @@ public class ProcesoController {
 	
 	@Autowired
 	private TipoCambioService tipoCambioService;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	/* Servicios para la Entidad TipoCambio */
 	@ApiOperation(value = "Crea un Tipo de Cambio", authorizations = {@Authorization(value = "apiKey") })	
@@ -208,5 +213,157 @@ public class ProcesoController {
 		}		
 	}
 	/* Fin Servicios para la Entidad TipoCambio */
+	
+	/* Servicios para la Entidad Cliente */
+	@ApiOperation(value = "Crea un Cliente", authorizations = {@Authorization(value = "apiKey") })	
+	@PostMapping(value = "/cliente")
+	public ResponseEntity<?> saveCliente(@RequestBody Cliente cliente, HttpServletRequest request){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Integer idUser = 0;
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			JwtUser userDetails = (JwtUser) auth.getPrincipal();
+			idUser = userDetails.getId();
+
+		} else {
+			return new ResponseEntity<>(new RestException("No autorizado"), HttpStatus.UNAUTHORIZED);
+		}
+		
+		HashMap<String, Object> result = new HashMap<>();		
+		cliente.setEstado(true);
+		cliente.setUserCr(idUser);
+		Cliente data = clienteService.insert(cliente);
+		
+		
+		result.put("success", true);
+		result.put("message", "Se ha registrado correctamente.");
+		result.put("result", data);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Lista todas los Clientes", authorizations = {@Authorization(value = "apiKey") })
+	@GetMapping(value = "/cliente")	
+	public ResponseEntity<?> findAllCliente(
+			@RequestParam(value = "tipo", required = false, defaultValue = "grilla") String tipo,
+			@RequestParam(value = "query", required = false, defaultValue = "") String query,
+			@RequestParam(value = "page", required = false, defaultValue = "-1") int page,
+			@RequestParam(value = "limit", required = false, defaultValue = "-1") int limit,
+			@RequestParam(value = "sortBy", required = false, defaultValue = "") String sortBy,
+			HttpServletRequest request) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Integer idUser = 0;
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			JwtUser userDetails = (JwtUser) auth.getPrincipal();
+			idUser = userDetails.getId();
+
+		} else {
+			return new ResponseEntity<>(new RestException("No autorizado"), HttpStatus.UNAUTHORIZED);
+		}
+		
+		if(tipo.equals("grilla")) {
+			int maxPage = 10;
+			
+			if (page == -1 && limit == -1 && "".equals(sortBy)) {
+				page = 1;
+				limit = maxPage;		
+			}else if (limit != -1 && page == -1) {
+				page = 1;
+			} else if (page != -1 && limit == -1) {
+				limit = maxPage;
+			}
+			
+			return new ResponseEntity<>(clienteService.findAll(query, page, limit, sortBy), HttpStatus.OK);
+		}else {			
+			return new ResponseEntity<>(clienteService.findAll(query, sortBy), HttpStatus.OK);
+		}
+	}
+	
+	@ApiOperation(value = "Obtiene datos de un Cliente", authorizations = { @Authorization(value = "apiKey")})
+	@GetMapping(value = "/cliente/{idCliente}")
+	public ResponseEntity<?> findCliente(@PathVariable(value = "idCliente") Integer idCliente, HttpServletRequest request) {
+		HashMap<String, Object> result = new HashMap<>();
+		Cliente data = clienteService.findById(idCliente);
+		if(data == null) {
+			result.put("success", false);
+			result.put("message", "No existe el Cliente con código: " + idCliente);
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		
+		result.put("success", true);
+		result.put("message", "Se ha encontrado el registro.");
+		result.put("result", data);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Actualiza un Cliente", authorizations = { @Authorization(value = "apiKey")})
+	@PutMapping(value = "/cliente")
+	public ResponseEntity<?> updateCliente (@RequestBody Cliente cliente, HttpServletRequest request) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Integer idUser = 0;
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			JwtUser userDetails = (JwtUser) auth.getPrincipal();
+			idUser = userDetails.getId();
+
+		} else {
+			return new ResponseEntity<>(new RestException("No autorizado"), HttpStatus.UNAUTHORIZED);
+		}
+		
+		HashMap<String, Object> result = new HashMap<>();
+		Cliente data = clienteService.findById(cliente.getIdCliente());
+		if(data == null) {
+			result.put("success", false);
+			result.put("message", "No existe el Cliente con código: " + cliente.getIdCliente());
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		try {
+			cliente.setUpAt(new java.sql.Timestamp(System.currentTimeMillis()));
+			cliente.setUserUp(idUser);
+			cliente.setEstado(true);
+			clienteService.update(cliente);
+			result.put("success", true);
+			result.put("message", "Se ha actualizado los datos del registro.");
+			result.put("result", cliente);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+			
+		} catch (Exception ex) {
+			return new ResponseEntity<>(new ApiCheckMoneyException(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}			
+	}
+	
+	@ApiOperation(value = "Elimina un Cliente", authorizations = { @Authorization(value = "apiKey")})
+	@DeleteMapping(value = "/cliente/{idCliente}")
+	public ResponseEntity<?> deleteCliente (@PathVariable(value = "idCliente") Integer idCliente, HttpServletRequest request){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Integer idUser = 0;
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			JwtUser userDetails = (JwtUser) auth.getPrincipal();
+			idUser = userDetails.getId();
+
+		} else {
+			return new ResponseEntity<>(new RestException("No autorizado"), HttpStatus.UNAUTHORIZED);
+		}
+		
+		HashMap<String, Object> result = new HashMap<>();
+		Cliente data = clienteService.findById(idCliente);
+		if(data == null) {
+			result.put("success", false);
+			result.put("message", "No existe el Cliente con código: " + idCliente);
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		try {
+			data.setDtAt(new java.sql.Timestamp(System.currentTimeMillis()));
+			data.setUserDt(idUser);
+			data.setEstado(false);
+			clienteService.delete(data);
+			result.put("success", true);
+			result.put("message", "Se ha eliminado los datos del registro.");
+			result.put("result", data);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+			
+		} catch (Exception ex) {
+			return new ResponseEntity<>(new ApiCheckMoneyException(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}		
+	}
+	/* Fin Servicios para la Entidad Cliente */
 
 }
