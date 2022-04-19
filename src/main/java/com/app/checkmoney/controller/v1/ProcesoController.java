f1bfdbba7cd20ec589bcb86cd85210c70b914bf6
Escudero183 <49972636+Escudero183@users.noreply.github.com>
2022-04-19 12:05:31 -0500

    Add microservicios Transaccion
    Se desarrolló los microservicios de Transaccion: get, post, put, delete

--- src/main/java/com/app/checkmoney/controller/v1/ProcesoController.java
+++ src/main/java/com/app/checkmoney/controller/v1/ProcesoController.java
@@ -3,6 +3,7 @@
  */
 package com.app.checkmoney.controller.v1;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -31,8 +32,10 @@ import com.app.checkmoney.config.exception.RestException;
 import com.app.checkmoney.config.security.model.JwtUser;
 import com.app.checkmoney.model.Cliente;
 import com.app.checkmoney.model.TipoCambio;
+import com.app.checkmoney.model.Transaccion;
 import com.app.checkmoney.service.ClienteService;
 import com.app.checkmoney.service.TipoCambioService;
+import com.app.checkmoney.service.TransaccionService;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
@@ -51,6 +54,177 @@ public class ProcesoController {
 	
 	@Autowired
 	private ClienteService clienteService;
+	
+	@Autowired
+	private TransaccionService transaccionService;
+	
+	/* Servicios para la Entidad Transaccion */
+	@ApiOperation(value = "Crea una Transaccion", authorizations = {@Authorization(value = "apiKey") })	
+	@PostMapping(value = "/transaccion")
+	public ResponseEntity<?> saveTransaccion(@RequestBody Transaccion transaccion, HttpServletRequest request){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Integer idUser = 0;
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			JwtUser userDetails = (JwtUser) auth.getPrincipal();
+			idUser = userDetails.getId();
+
+		} else {
+			return new ResponseEntity<>(new RestException("No autorizado"), HttpStatus.UNAUTHORIZED);
+		}
+		
+		HashMap<String, Object> result = new HashMap<>();
+		
+		Cliente cliente = clienteService.findById(transaccion.getCliente().getIdCliente());
+		if(cliente == null) {
+			result.put("success", false);
+			result.put("message", "No existe el Cliente con código:" + transaccion.getCliente().getIdCliente());
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		
+		TipoCambio tipoCambio = tipoCambioService.findById(transaccion.getTipoCambio().getIdTipoCambio());
+		if(tipoCambio == null) {
+			result.put("success", false);
+			result.put("message", "No existe el Tipo de Cambio con código:" + transaccion.getTipoCambio().getIdTipoCambio());
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		
+		transaccion.setEstado(true);
+		transaccion.setUserCr(idUser);
+		Transaccion data = transaccionService.insert(transaccion);
+		
+		
+		result.put("success", true);
+		result.put("message", "Se ha registrado correctamente.");
+		result.put("result", data);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Lista todas las Transacciones", authorizations = {@Authorization(value = "apiKey") })
+	@GetMapping(value = "/transaccion")	
+	public ResponseEntity<?> findAllTransaccion(
+			@RequestParam(value = "fechaOperacion", required = false, defaultValue = "") String fechaOperacion,
+			@RequestParam(value = "tipo", required = false, defaultValue = "grilla") String tipo,
+			@RequestParam(value = "query", required = false, defaultValue = "") String query,
+			@RequestParam(value = "page", required = false, defaultValue = "-1") int page,
+			@RequestParam(value = "limit", required = false, defaultValue = "-1") int limit,
+			@RequestParam(value = "sortBy", required = false, defaultValue = "") String sortBy,
+			HttpServletRequest request) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Integer idUser = 0;
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			JwtUser userDetails = (JwtUser) auth.getPrincipal();
+			idUser = userDetails.getId();
+
+		} else {
+			return new ResponseEntity<>(new RestException("No autorizado"), HttpStatus.UNAUTHORIZED);
+		}
+		
+		if(tipo.equals("grilla")) {
+			int maxPage = 10;
+			
+			if (page == -1 && limit == -1 && "".equals(sortBy)) {
+				page = 1;
+				limit = maxPage;		
+			}else if (limit != -1 && page == -1) {
+				page = 1;
+			} else if (page != -1 && limit == -1) {
+				limit = maxPage;
+			}
+			
+			return new ResponseEntity<>(transaccionService.findAll(fechaOperacion, query, page, limit, sortBy), HttpStatus.OK);
+		}else {			
+			return new ResponseEntity<>(transaccionService.findAll(query, sortBy), HttpStatus.OK);
+		}
+	}
+	
+	@ApiOperation(value = "Obtiene datos de una Transaccion", authorizations = { @Authorization(value = "apiKey")})
+	@GetMapping(value = "/transaccion/{idTransaccion}")
+	public ResponseEntity<?> findTransaccion(@PathVariable(value = "idTransaccion") Integer idTransaccion, HttpServletRequest request) {
+		HashMap<String, Object> result = new HashMap<>();
+		Transaccion data = transaccionService.findById(idTransaccion);
+		if(data == null) {
+			result.put("success", false);
+			result.put("message", "No existe la Transaccion con código: " + idTransaccion);
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		
+		result.put("success", true);
+		result.put("message", "Se ha encontrado el registro.");
+		result.put("result", data);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Actualiza una Transaccion", authorizations = { @Authorization(value = "apiKey")})
+	@PutMapping(value = "/transaccion")
+	public ResponseEntity<?> updateTransaccion (@RequestBody Transaccion transaccion, HttpServletRequest request) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Integer idUser = 0;
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			JwtUser userDetails = (JwtUser) auth.getPrincipal();
+			idUser = userDetails.getId();
+
+		} else {
+			return new ResponseEntity<>(new RestException("No autorizado"), HttpStatus.UNAUTHORIZED);
+		}
+		
+		HashMap<String, Object> result = new HashMap<>();
+		Transaccion data = transaccionService.findById(transaccion.getIdTransaccion());
+		if(data == null) {
+			result.put("success", false);
+			result.put("message", "No existe la Transaccion con código: " + transaccion.getIdTransaccion());
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		try {
+			transaccion.setUpAt(new java.sql.Timestamp(System.currentTimeMillis()));
+			transaccion.setUserUp(idUser);
+			transaccion.setEstado(true);
+			transaccionService.update(transaccion);
+			result.put("success", true);
+			result.put("message", "Se ha actualizado los datos del registro.");
+			result.put("result", transaccion);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+			
+		} catch (Exception ex) {
+			return new ResponseEntity<>(new ApiCheckMoneyException(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}			
+	}
+	
+	@ApiOperation(value = "Elimina una Transaccion", authorizations = { @Authorization(value = "apiKey")})
+	@DeleteMapping(value = "/transaccion/{idTransaccion}")
+	public ResponseEntity<?> deleteTransaccion (@PathVariable(value = "idTransaccion") Integer idTransaccion, HttpServletRequest request){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Integer idUser = 0;
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			JwtUser userDetails = (JwtUser) auth.getPrincipal();
+			idUser = userDetails.getId();
+
+		} else {
+			return new ResponseEntity<>(new RestException("No autorizado"), HttpStatus.UNAUTHORIZED);
+		}
+		
+		HashMap<String, Object> result = new HashMap<>();
+		Transaccion data = transaccionService.findById(idTransaccion);
+		if(data == null) {
+			result.put("success", false);
+			result.put("message", "No existe la Transaccion con código: " + idTransaccion);
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		try {
+			data.setDtAt(new java.sql.Timestamp(System.currentTimeMillis()));
+			data.setUserDt(idUser);
+			data.setEstado(false);
+			transaccionService.delete(data);
+			result.put("success", true);
+			result.put("message", "Se ha eliminado los datos del registro.");
+			result.put("result", data);
+			return new ResponseEntity<>(result, HttpStatus.OK);
+			
+		} catch (Exception ex) {
+			return new ResponseEntity<>(new ApiCheckMoneyException(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+		}		
+	}
+	/* Fin Servicios para la Entidad Transaccion */
 	
 	/* Servicios para la Entidad TipoCambio */
 	@ApiOperation(value = "Crea un Tipo de Cambio", authorizations = {@Authorization(value = "apiKey") })	
@@ -140,6 +314,27 @@ public class ProcesoController {
 		result.put("success", true);
 		result.put("message", "Se ha encontrado el registro.");
 		result.put("result", data);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Obtiene datos de un Tipo de Cambio Vigente por Fecha y Divisas", authorizations = { @Authorization(value = "apiKey")})
+	@GetMapping(value = "/tipo_cambio/vigente/{fecha}/{idDivisaOrigen}/{idDivisaDestino}")
+	public ResponseEntity<?> findTipoCambio(
+			@PathVariable(value = "fecha") Date fecha,
+			@PathVariable(value = "idDivisaOrigen") Integer idDivisaOrigen,
+			@PathVariable(value = "idDivisaDestino") Integer idDivisaDestino,
+			HttpServletRequest request) {
+		HashMap<String, Object> result = new HashMap<>();
+		List<TipoCambio> data = tipoCambioService.findVigentes(fecha, idDivisaOrigen, idDivisaDestino);
+		if(!data.isEmpty()) {
+			result.put("success", false);
+			result.put("message", "No existe un Tipo de Cambio vigente para fecha: " + fecha.toString());
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND); 
+		}
+		
+		result.put("success", true);
+		result.put("message", "Se ha encontrado el registro.");
+		result.put("result", data.get(0));
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 	
